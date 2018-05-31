ba6acc3f837b9d40245518dfed295256e4f2af51
BOUETJACKIE <jackieboue@hotmail.fr>
2018-05-31 17:04:32 +0200

    version 2

--- algoexercice1/Program.cs
+++ algoexercice1/Program.cs
@@ -11,5 +11,36 @@ namespace algoexercice1
         static void Main(string[] args)
         {
         }
+        private static int GererMenu(List<MenuItem> menu)
+        {
+            foreach (MenuItem ligne in menu)
+            {
+                Console.WriteLine($"{ligne.Numero}.{ligne.Libelle}");
+            }
+            int choix;
+
+            choix = int.Parse(Console.ReadLine());
+            foreach (MenuItem item in menu)     
+            {
+                if (choix == item.Numero)
+
+                    return choix;
+            }
+            // ou bien return menu.SingleOrDefault(x => x.Numero == choix) != null ? choix : -1;
+
+            Console.WriteLine("Erreur de saisie");
+            return -1;
+            
+        }
+        
     }
+    class MenuItem
+    {
+        public int Numero { get; set; }
+        public string Libelle { get; set; }
+    }
+
+
+
+
 }

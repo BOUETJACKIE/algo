bdb573154ec04ec717d701e50cdfd391ffbae709
BOUETJACKIE <jackieboue@hotmail.fr>
2018-06-01 15:52:03 +0200

    dernière version du 01/06

--- algoexercice1/MenuPerso/Menu.cs
+++ algoexercice1/MenuPerso/Menu.cs
@@ -5,8 +5,84 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace algoexercice1.MenuPerso
+
+
 {
+
+    /// <summary>
+    /// pour faire un menu 
+    /// </summary>
     class Menu
+
     {
+        private List<MenuItem> menuItems;
+
+        internal void InsererLigne(MenuItem menuItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        // si type.(List)...valeur null, si type int valeur 0
+        public Menu()
+        {
+            menuItems = new List<MenuItem>();
+
+        }
+        /// <summary>
+        /// insère un menuItem dans le menu 
+        /// </summary>
+        /// <param name="item">element à inserer dans le menu</param>
+        /// <exception cref="ArgumentException">si le numéro de l'item est déjà présent dans le menu </exception>    
+
+        public void InserLigne(MenuItem item)
+        {
+            foreach (var ligne in menuItems)
+            {
+                if (ligne.Numero == item.Numero)
+                { throw new ArgumentException($"Le numéro {item.Numero}existe déjà dans le menu "); }
+            }
+            // 2eme version: menuItems.Exists(x=>x.Numeroitem.Numero)){ throw new ArgumentException($"Le numéro {item.Numero}existe déjà dans le menu "); }
+            menuItems.Add(item);
+
+        }
+
+        /// <summary>
+        /// affiche les éléments du menu
+        /// </summary>
+        public void AfficherMenu()
+        {
+            foreach (MenuItem ligne in menuItems)
+            {
+                Console.WriteLine($"{ligne.Numero}.{ligne.Libelle}");
+            }
+        }
+
+        /// <summary>
+        /// récupération du choix de l'utilisa  teur par rapport aux éléments
+        /// </summary>
+        /// <returns>choix de l'utilisateur</returns>
+        public int Choisir()
+        {
+            Console.WriteLine("saisir un choix");
+            int choix;
+
+            choix = int.Parse(Console.ReadLine());
+
+
+            while (true)
+            {
+                foreach (MenuItem item in menuItems)
+                {
+                    if (choix == item.Numero)
+                    {
+
+                        return choix;
+                    }
+                }
+
+                Console.WriteLine("Erreur, saisir un choix:");
+            }
+        }
     }
+       
 }
--- algoexercice1/Program.cs
+++ algoexercice1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using algoexercice1.MenuPerso;
 
 namespace algoexercice1
 {
@@ -10,68 +11,29 @@ namespace algoexercice1
     {
         static void Main(string[] args)
         {
-            //création d'une nouvelle liste + intégration des nouveaux élements
-            List<MenuItem> list = new List<MenuItem>
+            Menu menu = new Menu();
             {
-             new MenuItem{Numero=1, Libelle = "menu 1" },
-             new MenuItem{Numero=2, Libelle = "menu 2" },
-             new MenuItem{Numero=3, Libelle = "menu 3" },
-             new MenuItem{Numero=4, Libelle = "menu 4" },
-             new MenuItem{Numero=5, Libelle = "menu 5" },
-             new MenuItem{Numero=6, Libelle = "menu 6" },
-            };
-            // création d'une variable (résultat) pour afficher la liste
-            int resultat = GererMenu(list);
-            Console.WriteLine(resultat);
-            Console.ReadLine();
-        }
-        /// <summary>
-        /// affiche une menu et récupère la saisie de l'utilisateur
-        /// </summary>
-        /// <param name="menu">la liste des choix possibles</param>
-        /// <returns>le choix de l'utilisateur</returns>
-        private static int GererMenu(List<MenuItem> menu)
-        {
-            //affiche les élements du menu
-            foreach (MenuItem ligne in menu)
-            {
-                Console.WriteLine($"{ligne.Numero}.{ligne.Libelle}");
+                menu.InsererLigne(new MenuItem { Numero = 1, Libelle = "menu 1" });
+                menu.InsererLigne(new MenuItem { Numero = 2, Libelle = "menu 2" });
+                menu.InsererLigne(new MenuItem { Numero = 3, Libelle = "menu 3" });
+                menu.InsererLigne(new MenuItem { Numero = 4, Libelle = "menu 4" });
+                menu.InsererLigne(new MenuItem { Numero = 5, Libelle = "menu 5" });
+                menu.InsererLigne(new MenuItem { Numero = 6, Libelle = "menu 6" });
+
+                menu.AfficherMenu();
+
+                int resultat = menu.Choisir();
+                Console.WriteLine(resultat);
+                Console.ReadLine();
             }
-            // récupération du choix de l'utilisateur
-            Console.WriteLine("saisir un choix");
-            int choix;
-
-            choix = int.Parse(Console.ReadLine());
-
-            //test du choix par rapport aux élément du menu
-            while (true)
-            {
-                foreach (MenuItem item in menu)
-                {
-                    if (choix == item.Numero)
-                    {
-
-                        return choix;
-                    }
-                }
-                // ou bien return menu.SingleOrDefault(x => x.Numero == choix) != null ? choix : -1;
-
-                Console.WriteLine("Erreur, saisir un choix:");
-            }
-            //return -1;
-
+            
         }
+       
+
+        
 
     }
-    /// <summary>
-    /// représente une ligne d'un menu 
-    /// </summary>
-    class MenuItem
-    {
-        public int Numero { get; set; }
-        public string Libelle { get; set; }
-
-    }
+    
 
 
 
